9e71dea5c127dba2320f1fc52071585a9663811d
Ben Boyle <benjamins.boyle@gmail.com>
2016-11-07 00:38:34 +1000

    redirect index to inbox view

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const ReactDOM = require( 'react-dom' );
 const router = require( 'react-router' );
 const Router = router.Router;
 const Route = router.Route;
+const IndexRedirect = router.IndexRedirect;
 
 const App = require( './components/App' );
 const MailContainer = require( './components/MailContainer' );
@@ -13,6 +14,7 @@ const Mail = require( './components/Mail' );
 const routes = (
 	<Router history={ router.hashHistory }>
         <Route path="/" component={ App }>
+            <IndexRedirect to="/inbox" />
             <Route path=":mailboxId" component={ MailboxContainer }>
             	<Route path=":mailId" component={ Mail } />
             </Route>
